6fa099a428fe5cbef80e8eb46e069ce9401e91bc
raju-tati <118584250+raju-tati@users.noreply.github.com>
2023-04-29 20:05:26 +0530

    Update blue.pl

--- blue.pl
+++ blue.pl
@@ -4,4 +4,49 @@ use experimentals;
 use threads;
 use Time::HiRes;
 
-say "hello";
+sub main() {
+    async {
+        say "printing inside a thread";
+    };
+}
+
+my $signalThread = async {
+    use sigtrap 'handler' => \&signalHandler, qw(INT);
+};
+# functionName: signalHandler().
+# handles the INT signal form keyboard.
+sub signalHandler($signalName) { 
+    say "got an intterupt, print some info";
+    my @threads = threads->list(threads::all);
+    say "remaining threads: ", scalar @threads;
+}
+my $monitorThread = async {
+    while(1) {
+        foreach my $thread (threads->list(threads::joinable)) {
+            $thread->detach();
+        }
+        Time::HiRes::sleep(0.005);
+    }
+};
+main();
+while(1) {
+    my @threads = threads->list(threads::all);
+    if(scalar @threads == 1) {
+        $monitorThread->detach();
+        last;
+    } else {
+        Time::HiRes::sleep(0.001);
+    }
+}
+exit();
+
+__END__
+=encoding utf8
+=pod
+=head1 FUNCTIONS
+
+    edit the functions without starting _.
+    functions starting with _ are private.
+    function staring with __ are for export.
+
+=cut
